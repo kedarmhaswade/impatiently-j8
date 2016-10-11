2674c3d65dc82fbe778e18c2e3edbf41ea4ee726
Kedar Mhaswade <kedar.mhaswade@gmail.com>
2016-10-11 15:56:45 -0700

    add a partition function

---
+++ src/test/java/practice/PartitionFunctionTest.java
@@ -0,0 +1,27 @@
+package practice;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+import static practice.PartitionFunction.nonIncreasing;
+
+/**
+ * Created by kedar on 10/11/16.
+ */
+public class PartitionFunctionTest {
+    @Test
+    public void nonIncreasingTest() throws Exception {
+        int n = 10;
+        LinkedList<Integer> rem = new LinkedList<>();
+        List<List<Integer>> acc = new ArrayList<>(8);
+        nonIncreasing(n, rem, acc);
+        assertEquals(42L, acc.size());
+        acc.forEach(list -> assertEquals(n, (long) list.stream().reduce(Integer::sum).get())); // deliberate non-use of ifPresent
+    }
+
+}
